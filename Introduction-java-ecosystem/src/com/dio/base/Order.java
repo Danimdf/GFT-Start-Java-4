5cf844899a1865b0807283a464c7bb41a8bd5b80
Daniela Monteiro <danimdf360@gmail.com>
2022-02-02 08:13:21 -0300

    Introduction Java ecosystem

---
+++ Introduction-java-ecosystem/src/com/dio/base/Order.java
@@ -0,0 +1,14 @@
+package com.dio.base;
+
+public class Order {
+
+    private final String code;
+
+    public Order(String code) {
+        this.code = code;
+    }
+
+    public String toString() {
+        return (code);
+    }
+}
