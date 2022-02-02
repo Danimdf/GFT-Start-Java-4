d484216ed9567e41d5399f0a1a44e160d97dd5f2
Daniela Monteiro <danimdf360@gmail.com>
2022-02-02 08:15:05 -0300

    Variables and Data types

---
+++ Data types/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,31 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte maxByte = 127;
+        byte minByte = -128;
+
+        short maxShort = 32767;
+        short minShort = -32768;
+
+        int maxInt = 2147483647;
+        int minInt = -2147483648;
+
+        long maxLong = 9223372036854775807L;
+        long minLong = -9223372036854775808L;
+
+        float maxFloat = 3.4028235E38f;
+        float minFloat = -3.4028235E38f;
+
+        double maxDouble = 1.7976931348623157E308d;
+        double minDouble = -1.7976931348623157E308d;
+
+        char maxChar =  '\uffff';
+        char minChar =  '\u0000';
+
+        boolean booTrue = true;
+        boolean booFalse = false;
+    }
+
+}
---
+++ Introduction-java-ecosystem copy/src/com/dio/MyFirstProgram.java
@@ -0,0 +1,12 @@
+package com.dio;
+
+import com.dio.base.Order;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        Order order = new Order ("Hello World!");
+
+        System.out.println(order);
+    }
+}
